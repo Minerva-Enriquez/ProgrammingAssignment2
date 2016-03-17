47214c8e1c5d68e6aca2844d9a05c68e3d2c94e6
Minerva Enriquez <mine.enriquez@hotmail.com>
2016-03-16 22:25:07 -0700

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,21 +1,7 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
-}
-
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
 
 ## --------------------------------------------
 ## Assignment: Caching the Inverse of a Matrix
+## Author: Minerva Enriquez
 ## --------------------------------------------
 
 ## 1.makeCacheMatrix: 
